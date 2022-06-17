1008b05ccf7c53265dae6b38b453c993229ad040
Stian Sørby <ssouthcity@gmail.com>
2022-06-17 21:03:42 +0200

    mvp

---
+++ mux.go
@@ -0,0 +1,27 @@
+package main
+
+import "github.com/bwmarrin/discordgo"
+
+type InteractionHandlerFunc func(s *discordgo.Session, i *discordgo.InteractionCreate)
+
+type Mux struct {
+	handlers map[string]InteractionHandlerFunc
+}
+
+func NewRouter() *Mux {
+	return &Mux{
+		handlers: make(map[string]InteractionHandlerFunc),
+	}
+}
+
+func (m *Mux) ApplicationCommand(name string, handler InteractionHandlerFunc) {
+	m.handlers[name] = handler
+}
+
+func (m *Mux) HandleInteraction(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	name := i.ApplicationCommandData().Name
+
+	if h, ok := m.handlers[name]; ok {
+		h(s, i)
+	}
+}
---
+++ mux_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func stubHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+}
+
+func TestRouter(t *testing.T) {
+	r := NewRouter()
+
+	t.Run("added routes are registered in mux", func(t *testing.T) {
+		commands := []string{"ping", "hello", "world"}
+
+		for _, cmd := range commands {
+			r.ApplicationCommand(cmd, stubHandler)
+		}
+
+		for _, cmd := range commands {
+			if _, ok := r.handlers[cmd]; !ok {
+				t.Error("handler was not added to struct")
+			}
+		}
+	})
+
+	t.Run("handler is called when interaction matches", func(t *testing.T) {
+		called := false
+		stubH := InteractionHandlerFunc(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			called = true
+		})
+
+		r.ApplicationCommand("pong", stubH)
+
+		r.HandleInteraction(&discordgo.Session{}, &discordgo.InteractionCreate{
+			Interaction: &discordgo.Interaction{
+				Type: discordgo.InteractionApplicationCommand,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "pong",
+				},
+			},
+		})
+
+		if !called {
+			t.Error("interaction handler for 'pong' was not called")
+		}
+	})
+
+	t.Run("handler is not called when interaction does not match", func(t *testing.T) {
+		called := false
+		stubH := InteractionHandlerFunc(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			called = true
+		})
+
+		r.ApplicationCommand("pong", stubH)
+
+		r.HandleInteraction(&discordgo.Session{}, &discordgo.InteractionCreate{
+			Interaction: &discordgo.Interaction{
+				Type: discordgo.InteractionApplicationCommand,
+				Data: discordgo.ApplicationCommandInteractionData{
+					Name: "ban",
+				},
+			},
+		})
+
+		if called {
+			t.Error("interaction handler for 'pong' was called when it shouldn't have been")
+		}
+	})
+}
